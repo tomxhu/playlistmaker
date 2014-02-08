ab0c0de92ef0ef0ad948f25ad0b3412ceae5f0ca
hustheman <hustheman739@gmail.com>
2014-02-08 14:36:34 -0500

    edited hosts again

--- routes/listener.js
+++ routes/listener.js
@@ -1,6 +1,6 @@
 //var db = require('mongoskin').db('localhost:27017/local', {safe:false});
 
-var db = require('mongoskin').db('admin:5Ct9vgx6M-g5@mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/playlist', {safe:false});
+var db = require('mongoskin').db('heroku_app22038962:testpass1@mongodb://<dbuser>:<dbpassword>@ds027789.mongolab.com:27789/heroku_app22038962', {safe:false});
 
 exports.home = function(req, res){
 	db.collection('queue').find().toArray(function(err, result) {
