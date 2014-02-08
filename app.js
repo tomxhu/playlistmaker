b7528214156b00c919e93316def8d4ea6ed25826
hustheman <hustheman739@gmail.com>
2014-02-08 14:13:33 -0500

    port 80

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
