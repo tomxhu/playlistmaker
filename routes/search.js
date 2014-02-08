23ad983a41205910e19f7a081bcbb0ad30035721
hustheman <hustheman739@gmail.com>
2014-02-08 14:46:31 -0500

    heroku

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,6 @@
 // var db = require('mongoskin').db('localhost:27017/local', {safe:false});
 
-var db = require('mongoskin').db('heroku_app22038962:testpass1@mongodb://<dbuser>:<dbpassword>@ds027789.mongolab.com:27789/heroku_app22038962', {safe:false});
+var db = require('mongoskin').db('heroku_app22038962:testpass1@ds027789.mongolab.com:27789/heroku_app22038962', {safe:false});
 
 exports.home = function(req, res){
   // get current queue data
