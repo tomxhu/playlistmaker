567896eda22afb13c6c7bfd5818dfd5399693777
hustheman <hustheman739@gmail.com>
2014-02-07 22:39:07 -0500

    setup for clevercloud

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+// Load the http module to create an http server.
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Beanpot\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
+// Last, but not least, listen on port 8080
+server.listen(8080);
