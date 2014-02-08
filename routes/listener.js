74a04bda791052cf5a2ae2f6468ce2d218c4e0a7
hustheman <hustheman739@gmail.com>
2014-02-08 15:25:12 -0500

    empty db bug

--- routes/listener.js
+++ routes/listener.js
@@ -1,18 +1,24 @@
-//var db = require('mongoskin').db('localhost:27017/local', {safe:false});
+var db = require('mongoskin').db('localhost:27017/local', {safe:false});
 
-var db = require('mongoskin').db('admin:password1@ds027759.mongolab.com:27759/heroku_app22039734', {safe:false});
+//var db = require('mongoskin').db('admin:password1@ds027759.mongolab.com:27759/heroku_app22039734', {safe:false});
 
 exports.home = function(req, res){
 	db.collection('queue').find().toArray(function(err, result) {
 			if (err) throw err;
 			console.log(result);
-			res.render('listener', { title: 'Listening Device', response: result[0].url});
-			db.collection('queue').remove(result[0],function(err){
+			if (result.length == 0){
+				console.log("empty");
+				res.render('listener', { title: 'Listening Device'});
+			} else {
+				res.render('listener', { title: 'Listening Device', response: result[0].url});
+				db.collection('queue').remove(result[0],function(err){
 					if (!err) console.log('entry deleted!');
 					db.collection('queue').find().toArray(function(err, result) {
 						console.log(result);
 					});
 				});
+			}
+			
 		});
 	
 };
