a60eca61a1c5636808238afb523eb020a496f7eb
hustheman <hustheman739@gmail.com>
2014-02-07 22:42:57 -0500

    Beanpot!

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var http = require('http');
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+  response.end("Hello HackBeanpot\n");
 });
 
 // Last, but not least, listen on port 8080
