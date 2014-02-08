d6913b181f10bd4b7b400fd64eccdb987392972c
hustheman <hustheman739@gmail.com>
2014-02-08 11:28:05 -0500

    remove from queue on refresh

--- routes/listener.js
+++ routes/listener.js
@@ -1,4 +1,17 @@
+var db = require('mongoskin').db('localhost:27017/local', {safe:false});
+
 exports.home = function(req, res){
-  res.render('listener', { title: 'Listening Device' })
+	db.collection('queue').find().toArray(function(err, result) {
+			if (err) throw err;
+			console.log(result);
+			res.render('listener', { title: 'Listening Device', response: result[0].url});
+			db.collection('queue').remove(result[0],function(err){
+					if (!err) console.log('entry deleted!');
+					db.collection('queue').find().toArray(function(err, result) {
+						console.log(result);
+					});
+				});
+		});
+	
 };
 
