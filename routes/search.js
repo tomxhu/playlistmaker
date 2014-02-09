aad133280f4bf6633355e23d8d3c746631677761
Matthew Coleman <kirocuto@gmail.com>
2014-02-08 20:36:30 -0500

    Got Multiview working
    multiview works, can't play anything but that should be fixed by merging

--- routes/search.js
+++ routes/search.js
@@ -1,5 +1,6 @@
 var db = require('mongoskin').db('localhost:27017/local', {safe:false});
 
+
 exports.home = function(req, res){
   // get current queue data
   res.render('search', { title: 'User Device' }) // pass as a var 
--- routes/user.js
+++ routes/user.js
@@ -6,10 +6,43 @@ exports.home = function(req, res){
 	res.render('user', { title: 'User Device' }) // pass as a var 
 };
 
+exports.home_get_handler = function(req, res){
+	if(req.body.add){
+		var to_add = req.body.add;
+		alert(to_add);
+
+		/*
+		db.collection('queue').insert({type: "Youtube", url: link});
+		db.collection('queue').find().toArray(function(err, result) {
+			if (err) throw err;
+			console.log(result);
+		});*/
+	}
+	/*var search = req.body.search;
+	req.session.search = search;
+	var id = req.body.id;
+
+	if (search != ''){
+		console.log(req.body.search + "req.body");
+		console.log(req.session.search + "req.session");
+		res.redirect('/user/search')
+	} else {
+		// remove id from queue
+	}*/
+}
+
 exports.home_post_handler = function(req, res){
 	var search = req.body.search;
 	req.session.search = search;
 	var id = req.body.id;
+
+	if(req.body.link && req.body.type){
+		db.collection('queue').insert({type: req.body.type, url: req.body.type});
+		db.collection('queue').find().toArray(function(err, result) {
+			if (err) throw err;
+			console.log(result);
+		});
+	}
 
 	if (search != ''){
 		console.log(req.body.search + "req.body");
@@ -32,22 +65,21 @@ exports.search = function(req, res){
 
 
 		// parse json for url to store
-		var vid = bodyJSON.feed.entry[0].id.$t
+		var videos = new Array();
 
-		var split = vid.split("videos\/");
-		var link = "http://www.youtube.com/embed/" + split[1] + "?autoplay=1";
+		for (var i = 0; i < bodyJSON.feed.entry.length; i++) {
+			var current = bodyJSON.feed.entry[i];
+			var vid = current.id.$t
+			var split = vid.split("videos\/");
+			var current_vid = {link : "http://www.youtube.com/embed/" + split[1] + "?autoplay=1",
+				thumb : current.media$group.media$thumbnail[3].url, 
+				vtitle : current.title.$t,
+				type : "Youtube"
+			};
+			videos.push(current_vid);
+		};
 
-		var thumb = bodyJSON.feed.entry[0].media$group.media$thumbnail[3].url;
-		var vtitle = bodyJSON.feed.entry[0].title.$t;
-
-		// adds url to db
-		db.collection('queue').insert({type: "Youtube", url: link});
-		db.collection('queue').find().toArray(function(err, result) {
-			if (err) throw err;
-			console.log(result);
-		});
-
-		res.render('search', { title: 'Search', query: search, response: thumb, vtitle: vtitle});
+		res.render('search', { title: 'Search', query: search, response: videos});
 		}
 	});
 	
